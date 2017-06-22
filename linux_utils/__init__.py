cb57a17299ef22388697b446d50781ed21810eed
Peter Odding <peter@peterodding.com>
2017-06-22 15:51:23 +0200

    Release 0.4: Command line interface for cryptdisks_{start,stop} emulation

--- linux_utils/__init__.py
+++ linux_utils/__init__.py
@@ -1,7 +1,7 @@
 # linux-utils: Linux system administration tools for Python.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: June 21, 2017
+# Last Change: June 22, 2017
 # URL: https://linux-utils.readthedocs.io
 
 """Linux system administration tools for Python."""
@@ -16,7 +16,7 @@ from executor.contexts import AbstractContext, LocalContext
 from humanfriendly import parse_size
 from six import string_types
 
-__version__ = '0.3'
+__version__ = '0.4'
 """Semi-standard module versioning."""
 
 
