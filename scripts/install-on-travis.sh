4e84eb256bd767f099641b03d56fd078e019cca3
Peter Odding <peter@peterodding.com>
2018-05-19 00:18:09 +0200

    Simplify install-on-travis.sh by adding requirements-travis.txt

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -2,10 +2,7 @@
 
 # Install the required Python packages.
 pip install pip-accel
-pip-accel install coveralls
-pip-accel install --requirement=requirements.txt
-pip-accel install --requirement=requirements-checks.txt
-pip-accel install --requirement=requirements-tests.txt
+pip-accel install --requirement=requirements-travis.txt
 
 # Install the project itself, making sure that potential character encoding
 # and/or decoding errors in the setup script are caught as soon as possible.
